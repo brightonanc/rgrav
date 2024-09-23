0013ce847455a0dc2fc7deba28265daa4176d351
Alex Saad-Falcon <alexsaadfalcon@yahoo.com>
2024-09-22 21:26:08 -0400

    Rework clustering a bit, generic averaging algo

--- src/rgrav_clustering.py
+++ src/rgrav_clustering.py
@@ -4,10 +4,11 @@ from tqdm import tqdm
 from .rgrav import *
 from .util import *
 
-class RGravClustering:
-    def __init__(self):
-        self.rgrav = RGrAv()
+class SubspaceClustering:
+    def __init__(self, ave_algo):
+        self.ave_algo = ave_algo
         self.n_iters = 100
+        self.center_tol = 1e-2
 
     def cluster(self, points, n_centers):
         # clustering pseudocode:
@@ -15,8 +16,8 @@ class RGravClustering:
         # 2. assign points to centers
         # 3. update centers with RGrAv
         # 4. repeat until convergence
-        import matplotlib.pyplot as plt
-        losses = []
+
+        changes = []
         U_centers = []
         # pick random points as initial centers
         for i in range(n_centers):
@@ -24,6 +25,13 @@ class RGravClustering:
             U_centers.append(points[ind])
 
         for i in tqdm(range(self.n_iters)):
+            # if two centers are too close, reinit one
+            for i in range(n_centers):
+                for j in range(i+1, n_centers):
+                    if grassmannian_dist(U_centers[i], U_centers[j]) < self.center_tol:
+                        ind = torch.randint(0, points.shape[0], (1,)).item()
+                        U_centers[i] = points[ind]
+
             # do cluster assignment
             dists = torch.stack([torch.stack([grassmannian_dist(point, center) for center in U_centers]) for point in points])
             cluster_assignments = torch.argmin(dists, dim=1)
@@ -33,16 +41,18 @@ class RGravClustering:
             for i in range(n_centers):
                 cluster_points = points[cluster_assignments == i]
                 if len(cluster_points) > 0:
-                    new_center = self.rgrav.average(cluster_points)
-                    center_changes.append(grassmannian_dist(U_centers[i], new_center))
-                    U_centers[i] = new_center
+                    new_center = self.ave_algo.average(cluster_points)
                 else:
-                    center_changes.append(torch.tensor(0.0))
+                    print('no points in cluster', i)
+                    ind = torch.randint(0, points.shape[0], (1,)).item()
+                    new_center = points[ind]
+                center_changes.append(grassmannian_dist(U_centers[i], new_center))
+                U_centers[i] = new_center
 
             # compute loss / check convergence
-            loss = sum(center_changes)
-            losses.append(loss)
-            if len(losses) > 1 and losses[-1] >= losses[-2]:
+            change = sum(center_changes)
+            changes.append(change)
+            if len(changes) > 1 and changes[-1] >= changes[-2]:
                 break
 
         return U_centers
@@ -86,7 +96,7 @@ if __name__ == "__main__":
     print('points shape', points.shape)
 
     # do clustering
-    rgrav_clustering = RGravClustering()
+    rgrav_clustering = SubspaceClustering(RGrAv())
     clusters = rgrav_clustering.cluster(points, n_centers)
 
     closest_centers = []
