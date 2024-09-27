35d8b7513aa1af9e39efeffe4579cb60c1c5f1cb
Saad-Falcon <asf3@gatech.edu>
2024-09-27 15:06:06 -0400

    Flag is ok for low dimension, but bad for high dimension/many points

--- scripts/benchmark_clustering.py
+++ scripts/benchmark_clustering.py
@@ -6,8 +6,8 @@ from src.util import grassmannian_dist_chordal
 from src.timing import time_func, TimeAccumulator
 
 
-N = 100; K = 10
-# N = 10; K = 3
+N = 100
+K = 10
 n_points = 100
 n_centers = 10
 center_dist = 1.0
