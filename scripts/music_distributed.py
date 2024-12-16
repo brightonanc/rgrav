a873143679a30102379e936d173e4bff8fa85562
Alex Saad-Falcon <alexsaadfalcon@yahoo.com>
2024-12-16 16:43:49 -0500

    MUSIC works with DRGrAv

--- scripts/music_distributed.py
+++ scripts/music_distributed.py
@@ -27,7 +27,7 @@ A2 = generate_narrowband_weights_azel(1, N, az, el).to(device)
 A2 /= torch.norm(A)
 A = torch.cat([A, A2], dim=1); k = 2
 
-def generate_sample(A, n, noise_level=1e-2):
+def generate_sample(A, n, noise_level=1e-1):
     S = randn_complex(A.shape[1], n, device)
     E = randn_complex(N, n, device) * np.sqrt(noise_level)
     return A @ S + E
@@ -39,7 +39,7 @@ R = X @ X.T.conj()
 U_est, _, _ = torch.linalg.svd(R)
 U_est = U_est[:, k:]
 
-def music_spec(U, npts=100):
+def music_spec(U, npts=1000):
     azs = torch.linspace(-30, 30, npts)
     el = torch.tensor(0)
     spec = []
@@ -52,5 +52,60 @@ def music_spec(U, npts=100):
 azs, spec = music_spec(U_est)
 
 plt.figure()
-plt.plot(azs, spec)
+plt.plot(azs, spec, label='Centralized')
+
+# Split data into 10 nodes
+# each node contains noise subspace for local data
+n_nodes = 10
+node_samples = X.shape[1] // n_nodes
+Us = []
+for i in range(n_nodes):
+    node_data = X[:, i*node_samples:(i+1)*node_samples]
+    node_R = node_data @ node_data.T.conj()
+    U, _, __ = torch.linalg.svd(node_R)
+    U = U[:, k:]
+    Us.append(U)
+Us = torch.stack(Us)
+
+# get average projector spectrum
+P = 0.
+for U in Us:
+    P += U @ U.T.conj()
+_, P_S, _ = torch.linalg.svd(P)
+
+# do consensus with DRGrAv
+max_iter = 10
+consensus = ChebyshevConsensus(
+    CycleGraph.get_positive_optimal_lapl_based_comm_W(n_nodes),
+    cons_rounds = max_iter,
+)
+
+# Run RGrAv
+kwargs = dict()
+alpha = 0.1
+rgrav = AsymptoticDRGrAv(alpha, consensus, **kwargs)
+U_rgrav = rgrav.average(Us, max_iter=max_iter)
+for i in range(U_rgrav.shape[0]):
+    print('RGrAv dist', (N - k) - torch.norm(U_rgrav[0].T @ U_rgrav[i]) ** 2)
+U_rgrav = U_rgrav[0]
+# U_rgrav = U_rgrav[:, :k] + 1j * U_rgrav[:, k:]
+U_rgrav = U_rgrav.to(torch.cfloat)
+
+azs, spec = music_spec(U_rgrav)
+plt.plot(azs, spec, label='DRGrAv')
+
+# run DEEPCA
+# deepca = DeEPCA(consensus) 
+# U_deepca = deepca.average(Us, max_iter=max_iter)
+# U_deepca = U_deepca[0]
+
+# azs, spec = music_spec(U_deepca)
+# plt.plot(azs, spec, label='DEEPCA')
+
+plt.legend()
+
+plt.figure()
+plt.suptitle('Projector Spectrum')
+plt.plot(P_S)
+
 plt.show()
